e8f1fb6ddcf883970093e9008a88df6117ce1866
Sinan Kaya <sinan.kaya@microsoft.com>
2019-02-16 20:36:11 +0000

    deprecated

--- src/vm/appdomain.hpp
+++ src/vm/appdomain.hpp
@@ -3441,8 +3441,13 @@ public:
 
     //****************************************************************************************
     // Methods used to get the callers module and hence assembly and app domain.
-    __declspec(deprecated("This method is deprecated, use the version that takes a StackCrawlMark instead"))
-    static Module* GetCallersModule(int skip);
+#if defined(_MSC_VER) || defined(__llvm__)
+    #define DEPRECATED(msg) __declspec(deprecated(msg))
+#else
+    #define DEPRECATED(msg) __attribute__((deprecated))
+#endif
+
+    static DEPRECATED("This method is deprecated, use the version that takes a StackCrawlMark instead")  Module* GetCallersModule(int skip);
     static MethodDesc* GetCallersMethod(StackCrawlMark* stackMark, AppDomain **ppAppDomain = NULL);
     static MethodTable* GetCallersType(StackCrawlMark* stackMark, AppDomain **ppAppDomain = NULL);
     static Module* GetCallersModule(StackCrawlMark* stackMark, AppDomain **ppAppDomain = NULL);
