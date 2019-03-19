a7b5464540c1039b1131fb2ea9f3124ee5781062
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-19 03:38:00 +0000

    find src/jit -type f -exec sed -i -e 's/genInterruptible->/getInterruptible()->/g' {} \;
    Cleanup remaining getInterruptible()

--- src/jit/morph.cpp
+++ src/jit/morph.cpp
@@ -16229,14 +16229,14 @@ void Compiler::fgSetOptions()
     if (JitConfig.JitFullyInt() || compStressCompile(STRESS_GENERIC_VARN, 30))
     {
         noway_assert(!codeGen->isGCTypeFixed());
-        genInterruptible = true;
+        setInterruptible(true);
     }
 #endif
 
     if (opts.compDbgCode)
     {
         assert(!codeGen->isGCTypeFixed());
-        genInterruptible = true; // debugging is easier this way ...
+        setInterruptible(true); // debugging is easier this way ...
     }
 
     /* Assume we won't need an explicit stack frame if this is allowed */
@@ -16299,7 +16299,7 @@ void Compiler::fgSetOptions()
     {
         assert(!codeGen->isGCTypeFixed());
         // Enforce fully interruptible codegen for funclet unwinding
-        genInterruptible = true;
+        setInterruptible(true);
     }
 #endif // UNIX_X86_ABI
 
@@ -16324,7 +16324,7 @@ void Compiler::fgSetOptions()
         // Even if there is no catch or other way to resume execution in this frame
         // the VM requires the security object to remain alive until later, so
         // Frames with security objects must be fully interruptible.
-        genInterruptible = true;
+        setInterruptible(true);
 
 #endif // JIT32_GCENCODER
     }
@@ -16345,7 +16345,7 @@ void Compiler::fgSetOptions()
         codeGen->setFramePointerRequiredGCInfo(true);
     }
 
-    // printf("method will %s be fully interruptible\n", genInterruptible ? "   " : "not");
+    // printf("method will %s be fully interruptible\n", getInterruptible() ? "   " : "not");
 }
 
 /*****************************************************************************/
