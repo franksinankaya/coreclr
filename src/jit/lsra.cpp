33c568aa235d94736ddfb08c0efa5d0a8201caa3
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-22 05:15:12 +0000

    find src/jit -type f -exec sed -i -e 's/->lvOtherReg/->GetOtherReg()/g' {} \;

--- src/jit/lsra.cpp
+++ src/jit/lsra.cpp
@@ -1612,7 +1612,7 @@ void LinearScan::identifyCandidates()
         // Initialize all variables to REG_STK
         varDsc->lvRegNum = REG_STK;
 #ifndef _TARGET_64BIT_
-        varDsc->lvOtherReg = REG_STK;
+        varDsc->GetOtherReg() = REG_STK;
 #endif // _TARGET_64BIT_
 
         if (!enregisterLocalVars)
