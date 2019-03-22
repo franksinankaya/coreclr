4c009d3ea58258f75b960b70b336ffe16e824fb3
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-22 05:15:12 +0000

    find src/jit -type f -exec sed -i -e 's/->lvOtherReg/->GetOtherReg()/g' {} \;

--- src/jit/codegenarmarch.cpp
+++ src/jit/codegenarmarch.cpp
@@ -2715,9 +2715,9 @@ void CodeGen::genJmpMethod(GenTree* jmp)
             getEmitter()->emitIns_S_R(INS_str, EA_4BYTE, varDsc->lvRegNum, varNum, 0);
 
             // Is the upper half also enregistered?
-            if (varDsc->lvOtherReg != REG_STK)
+            if (varDsc->GetOtherReg() != REG_STK)
             {
-                getEmitter()->emitIns_S_R(INS_str, EA_4BYTE, varDsc->lvOtherReg, varNum, sizeof(int));
+                getEmitter()->emitIns_S_R(INS_str, EA_4BYTE, varDsc->GetOtherReg(), varNum, sizeof(int));
             }
         }
         else
