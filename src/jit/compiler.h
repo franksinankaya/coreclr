0e67555dc8bc333290739abd927283ca1b0b2850
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 04:40:40 +0000

    __declspec(property(get = getFullPtrRegMap, put = setFullPtrRegMap)) bool genFullPtrRegMap;

--- src/jit/compiler.h
+++ src/jit/compiler.h
@@ -7395,7 +7395,6 @@ public:
                            unsigned refCntWtdStkDbl);
 #endif // DOUBLE_ALIGN
 
-    __declspec(property(get = getFullPtrRegMap, put = setFullPtrRegMap)) bool genFullPtrRegMap;
     bool getFullPtrRegMap()
     {
         return codeGen->genFullPtrRegMap;
