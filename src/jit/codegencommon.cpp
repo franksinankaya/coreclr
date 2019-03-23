cd1726bc7ddb889d6fcab7723fddcb895d10836e
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 03:36:15 +0000

    find src/jit -type f -exec sed -i -e 's/.*declspec.*hasTailCalls.*//g' {} \;

--- src/jit/codegencommon.cpp
+++ src/jit/codegencommon.cpp
@@ -159,7 +159,7 @@ CodeGen::CodeGen(Compiler* theCompiler) : CodeGenInterface(theCompiler)
 
     setInterruptible(false);
 #ifdef _TARGET_ARMARCH_
-    hasTailCalls = false;
+    setHasTailCalls(false);
 #endif // _TARGET_ARMARCH_
 #ifdef DEBUG
     genInterruptibleUsed = false;
@@ -8568,7 +8568,7 @@ void CodeGen::genFnEpilog(BasicBlock* block)
 
     if (jmpEpilog)
     {
-        hasTailCalls = true;
+        setHasTailCalls(true);
 
         noway_assert(block->bbJumpKind == BBJ_RETURN);
         noway_assert(block->getBBTreeList() != nullptr);
