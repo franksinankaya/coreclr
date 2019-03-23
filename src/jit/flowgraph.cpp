58397a134cd4c07d79f1b4db2354921687dda9ce
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:05:10 +0000

    find src/jit -type f -exec sed -i -e 's/gtArrLen\./AsArrLenRef()\./g' {} \;

--- src/jit/flowgraph.cpp
+++ src/jit/flowgraph.cpp
@@ -9588,7 +9588,7 @@ void Compiler::fgSimpleLowering()
                 case GT_ARR_LENGTH:
                 {
                     GenTreeArrLen* arrLen = tree->AsArrLen();
-                    GenTree*       arr    = arrLen->gtArrLen.ArrRef();
+                    GenTree*       arr    = arrLen->AsArrLenRef().ArrRef();
                     GenTree*       add;
                     GenTree*       con;
 
