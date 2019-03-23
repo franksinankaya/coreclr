a2155fa7825f722762d64d681f0b8eeffd13ba1e
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:39:40 +0000

    find src/jit -type f -exec sed -i -e 's/gtArrIndex\./AsArrIndex()\./g' {} \;

--- src/jit/valuenum.cpp
+++ src/jit/valuenum.cpp
@@ -9466,7 +9466,7 @@ void Compiler::fgValueNumberAddExceptionSet(GenTree* tree)
                 break;
 
             case GT_ARR_INDEX:
-                fgValueNumberAddExceptionSetForIndirection(tree, tree->gtArrIndex.ArrObj());
+                fgValueNumberAddExceptionSetForIndirection(tree, tree->AsArrIndex()->ArrObj());
                 break;
 
             case GT_ARR_OFFSET:
