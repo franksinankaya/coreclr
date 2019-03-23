fffd8994b867b7a6ffde17864f0c61fb184191f0
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:05:10 +0000

    find src/jit -type f -exec sed -i -e 's/gtArrLen\./AsArrLen()\./g' {} \;

--- src/jit/gentree.cpp
+++ src/jit/gentree.cpp
@@ -1243,7 +1243,7 @@ AGAIN:
             switch (oper)
             {
                 case GT_ARR_LENGTH:
-                    if (op1->gtArrLen.ArrLenOffset() != op2->gtArrLen.ArrLenOffset())
+                    if (op1->AsArrLen()->ArrLenOffset() != op2->AsArrLen()->ArrLenOffset())
                     {
                         return false;
                     }
@@ -1924,7 +1924,7 @@ AGAIN:
             switch (oper)
             {
                 case GT_ARR_LENGTH:
-                    hash += tree->gtArrLen.ArrLenOffset();
+                    hash += tree->AsArrLen()->ArrLenOffset();
                     break;
                 case GT_CAST:
                     hash ^= tree->gtCast.gtCastType;
@@ -7136,7 +7136,7 @@ GenTree* Compiler::gtCloneExpr(
             break;
 
             case GT_ARR_LENGTH:
-                copy = gtNewArrLen(tree->TypeGet(), tree->AsOp()->gtOp1, tree->gtArrLen.ArrLenOffset());
+                copy = gtNewArrLen(tree->TypeGet(), tree->AsOp()->gtOp1, tree->AsArrLen()->ArrLenOffset());
                 break;
 
             case GT_ARR_INDEX:
