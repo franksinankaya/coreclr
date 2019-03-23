25dbb037e59d6acc15c1e32a3be54e1a68985dd4
Sinan Kaya <sinan.kaya@microsoft.com>
2019-03-23 05:14:52 +0000

    find src/jit -type f -exec sed -i -e 's/gtRetExpr\./AsRetExprRef()\./g' {} \;

--- src/jit/gentree.h
+++ src/jit/gentree.h
@@ -6155,7 +6155,7 @@ inline GenTree* GenTree::gtRetExprVal()
     {
         if (retExprVal->gtOper == GT_RET_EXPR)
         {
-            retExprVal = retExprVal->gtRetExpr.gtInlineCandidate;
+            retExprVal = retExprVal->AsRetExprRef().gtInlineCandidate;
         }
         else
         {
